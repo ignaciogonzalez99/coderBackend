43b76e4ef3307c827909022e8f7eaacd21b71032
ignacio gonzalez <solrock12@gmail.com>
2022-08-01 05:45:40 -0300

    ok

---
+++ clase6/serverexpres/server.js
@@ -0,0 +1,22 @@
+const express =require('express');
+
+const app=express();
+let visitas=0;
+app.get('/',(req,res)=>{
+    res.send(`<h1 style="color:blue;">hola mundo</h1>`);
+});
+
+app.get('/visitas',(req,res)=>{
+    visitas+=1;
+    res.send(`la cantidad de visitas es:${visitas}`);
+});
+app.get('/fyh',(req,res)=>{
+    const date=new date
+    res.send(date.toLocaleString());
+});
+
+const server =app.listen(8080, ()=>{
+    console.log('esta esuchando correctamente ${server.address().port}')
+})
+
+server.on('error',error=>console.log('error en servidor ${error}'));
