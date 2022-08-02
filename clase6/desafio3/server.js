239ed913dfdd60140e86863dc581a6b363a0f6b4
ignacio gonzalez <solrock12@gmail.com>
2022-08-01 22:50:25 -0300

    ok

--- clase6/desafio3/server.js
+++ clase6/desafio3/server.js
@@ -1,15 +1,24 @@
+
+
 const express =require('express');
 const app=express();
 const Contenedor =require('./Contenedor.js');
 const product = new Contenedor(`./productos.txt`)
+
+
 app.get('/productos',async (req,res)=>{
     const item =await product.getAll()
     res.send(item);
 });
+
 app.get('/productoRandom',async (req,res)=>{
     const items = await product.getAll();
     const randomCheto = items[Math.floor(items.length*Math.random())]
-    res.send(randomCheto);// buen nombre le mando 
+    res.send(randomCheto);
 });
+
+const server =app.listen(8080, ()=>{
+    console.log(`esta esuchando correctamente ${server.address().port}`)
+})
 
 
